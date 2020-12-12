e06cedb23ad787ba189c99fa5f8a4714712c9581
José Ricardo David <david_Ricardo@live.com>
2020-12-12 14:17:00 -0300

    Initial Commit
    Pong game with Unity 2D

---
+++ Assets/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static int PlayerScore1 = 0;
+    public static int PlayerScore2 = 0;
+
+    public GUISkin layout;
+
+    GameObject theBall;
+
+    // Use this for initialization
+    void Start()
+    {
+        theBall = GameObject.FindGameObjectWithTag("Ball");
+    }
+
+    public static void Score(string wallID)
+    {
+        if (wallID == "RightWall")
+        {
+            PlayerScore1++;
+        }
+        else
+        {
+            PlayerScore2++;
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.skin = layout;
+        GUI.Label(new Rect(Screen.width / 2 - 100 - 12, 20, 50, 50), PlayerScore1.ToString());
+        GUI.Label(new Rect(Screen.width / 2 + 100 + 12, 20, 50, 50), PlayerScore2.ToString());
+
+        if (GUI.Button(new Rect(Screen.width / 2 - 35, 10, 90, 40), "RESTART"))
+        {
+            PlayerScore1 = 0;
+            PlayerScore2 = 0;
+            theBall.SendMessage("RestartGame", 0.5f, SendMessageOptions.RequireReceiver);
+        }
+
+        if (PlayerScore1 == 10)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER ONE WINS");
+            theBall.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
+        }
+        else if (PlayerScore2 == 10)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER TWO WINS");
+            theBall.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
+        }
+    }
+}
